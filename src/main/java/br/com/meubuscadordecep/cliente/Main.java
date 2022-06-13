be6ff80e4cda3965b7e5bd2ffe57d41f59feb882
Dario Ribeiro <darioribeiro1998@hotmail.com>
2022-06-12 21:31:21 -0300

    Initial commit

---
+++ src/main/java/br/com/meubuscadordecep/cliente/Main.java
@@ -0,0 +1,19 @@
+package br.com.meubuscadordecep.cliente;
+
+import br.com.meubuscadordecep.dominio.Endereco;
+import br.com.meubuscadordecep.viacep.ServicoDeCep;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Informe seu CEP: ");
+        String cep = new Scanner(System.in).nextLine();
+        Endereco endereco = ServicoDeCep.buscaEnderecoPelo(cep);
+
+        System.out.println("Logradouro: " + endereco.getLogradouro());
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Localidade: " + endereco.getLocalidade());
+    }
+
+}
